97fd20907ae1bf38be6809f7e854645a89553585
byamane <byamane21@gmail.com>
2022-02-04 17:16:03 -0600

    add getWinner functionality

--- js/app.js
+++ js/app.js
@@ -1,39 +1,6 @@
 /*-------------------------------- Constants --------------------------------*/
-const winCondition = [ //step 4
-	//Player 1
-		[1,0,0,
-		0,1,0,
-		0,0,1],
-		
-		[0,0,1,
-		0,1,0,
-		1,0,0],
-			
-		[0,1,0,
-		0,1,0,
-		0,1,0],
-			 
-		[0,0,0,
-		1,1,1,
-		0,0,0],
-		
-	//Player Two
-		[-1,0,0,
-		0,-1,0,
-		0,0,-1],
-			
-		[0,0,-1,
-		0,-1,0,
-		-1,0,0],
-			
-		[0,-1,0,
-		0,-1,0,
-		0,-1,0],
-			
-		[0,0,0,
-		-1,-1,-1,
-		0,0,0],
-	]
+const winningCombos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
 
 
 /*---------------------------- Variables (state) ----------------------------*/
@@ -62,6 +29,7 @@ function init(){
 }
 
 function render(){
+	getWinner()
 	board.forEach((square, index) => {
 	if (square === 1) {
 		squareEls[index].textContent = "X"
@@ -80,12 +48,10 @@ function render(){
 	else {
 		function playerName() {
 			let output;
-			if (turn === 1) {
+			if (turn === -1) {
 				output = 'Player 1';
-			} else if (turn === -1) {
+			} else if (turn === 1) {
 				output = 'Player 2';
-			} else {
-				output = 'Error in func playerName()';
 			}
 			return output;
 		}
@@ -105,44 +71,15 @@ function handleClick(evt){
 	render()
 }
 
-// function getWinner(){
-// 	winCondition.forEach(win){
-// 		if (board.some(win){
-// 			winner = turn
-// 		})
-
-// 	}
-// }
-
-
-
-
-	// 5.6) Set the winner variable if there's a winner by calling a new function: getWinner.
-	  // The getWinner function will...
-
-	  // 5.6.1) There are a couple methods you can use to find out if there is a winner.
-	   // This is the first, more elegant way that takes advantage of the winningCombos array you wrote above in step 4.
-	   // The 5.6.2 step is a little simpler to comprehend, but you'll need to write a lot more code.
-	   // The 5.6.2 step also won't take advantage of the winningCombos array, but using it as a reference will help you build a solution.
-	   // Choose only one path.
-		  // 5.6.1.1) Loop through the each of the winning combination arrays defined.
-		  // 5.6.1.2) Total up the three board positions using the three indexes in the current combo.
-		  // 5.6.1.3) Convert the total to an absolute value (convert any negative total to positive).
-		  // 5.6.1.4) If the total equals 3, we have a winner! Set the winner variable to the board's value at the index specified by the first index of that winning combination's array by returning that value.
-
-		// 5.6.2) This solution is less elegant, but might be easier to write on your own if you're struggling with the 5.6.1.X pseudocode.
-		  // 5.6.2.1) For each one of the winning combinations you wrote in step 4, find the total of each winning combination.
-		  // 5.6.2.2) Convert the total to an absolute value (convert any negative total to positive)
-		  // 5.6.2.3) If the total equals 3, we have a winner! Set the winner variable to the board's value at the index specified by the first index of that winning combination's array by returning that value.
-
-		// 5.6.3) Next, If there's no winner, check if there's a tie:
-
-		// 5.6.4) Set the winner varible to "T" if there are no more nulls in the board array by returning the string "T".
-	  
-		// 5.6.5) Otherwise return null.
-
-// 5.7) All state has been updated, so render the state to the page (step 3.3).
-
+function getWinner() {
+	winningCombos.forEach(combo => {
+  	if (Math.abs(board[combo[0]] + board[combo[1]] + board[combo[2]]) === 3){
+			winner = turn
+		} else if(!board.includes(null)){
+			winner = 'T'
+		}
+	})
+}
 
 // 6) Handle a player clicking the replay button:
 
